4ba1310a938217796c752f834b2bd3c89aa4bef3
Thomas Riffard <ferdodo@hotmail.fr>
2023-06-03 18:10:02 +0200

    add hint and share, clean active word list

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { wordOfTheDay$ } from "./wordOfTheDay";
 import { findAnagrams } from "./findAnagrams";
 import { printNextAnagrams } from "./printNextAnagrams";
 import { printWordList } from "./printWordList";
+import { wordHint } from "./wordHint";
 import "cookies-ds";
 
 globalThis.shufflet_printNextAnagrams = printNextAnagrams;
@@ -13,7 +14,7 @@ globalThis.shufflet_findAnagrams = findAnagrams;
 export const app = createApp({
 	setup() {
 		const wordOfTheDay: Ref<string | null> = ref(null);
-		const anagrams: Ref<any | null> = ref(null);
+		const anagrams: Ref<string[]> = ref([]);
 		const found: Ref<Record<string, boolean>> = ref({});
 
 		const wordOfTheDaySub = wordOfTheDay$.subscribe(function(value) {
@@ -21,13 +22,40 @@ export const app = createApp({
 			anagrams.value = findAnagrams(value);
 		});
 
-		function tryWord(word, anagram) {
-			if (word === anagram) {
+		function tryWord(word: string, i: number) {
+			if (anagrams.value.includes(word)) {
+				const previousPosition: number = anagrams.value.findIndex(w => w === word);
+
+				if (previousPosition === -1) {
+					throw new Error("Position of word not found !");
+				}
+				
+				const newAnagramsOrder: string[] = [...anagrams.value];
+				newAnagramsOrder.splice(previousPosition, 1);
+				newAnagramsOrder.splice(i, 0, word);
+				anagrams.value = newAnagramsOrder;
+			
 				found.value = {
 					...found.value,
 					[word]: true
 				};
 			}
+		}
+
+		function share() {
+			const date = new Date();
+			const year = date.getFullYear();
+			const month = ('0' + (date.getMonth() + 1)).slice(-2);
+			const day = ('0' + date.getDate()).slice(-2);
+			const formattedDate = `${year}/${month}/${day}`;
+			let text = `Shufflet ${formattedDate} - ${ wordOfTheDay.value }`;
+
+			for (const anagram of anagrams.value) {
+				text += `\n${ wordHint(anagram) }`
+			}
+
+			text += `\n\nhttps://ferdodo.github.io/shufflet`;
+			navigator.clipboard.writeText(text);
 		}
 
 		onUnmounted(wordOfTheDaySub.unsubscribe);
@@ -36,7 +64,9 @@ export const app = createApp({
 			wordOfTheDay,
 			anagrams,
 			tryWord,
-			found
+			found,
+			wordHint,
+			share
 		};
 	},
 	render
---
+++ wordHint.ts
@@ -0,0 +1,14 @@
+export function wordHint(str: string, percentage: number = 0.15): string {
+  if (str.length <= 2) {
+    return str;
+  }
+
+  const firstCharsCount = Math.ceil(str.length * percentage);
+  const lastCharsCount = Math.ceil(str.length * percentage);
+
+  const firstChars = str.substring(0, firstCharsCount);
+  const lastChars = str.substring(str.length - lastCharsCount);
+  const middleChars = '_'.repeat(str.length - firstCharsCount - lastCharsCount);
+
+  return `${firstChars}${middleChars}${lastChars}`;
+}
